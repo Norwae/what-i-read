2671b0d4aed95ff519ed4e4bcb1983a0c5441784
Stefan Schulz <schulz.stefan@gmail.com>
2013-10-18 16:07:47 +0200

    Simple DB queries against SQLite
    Still everything on UI Thread, but in theory, we could display
    some useful info now.

--- src/com/github/norwae/whatiread/db/BookDatabase.java
+++ src/com/github/norwae/whatiread/db/BookDatabase.java
@@ -1,6 +1,5 @@
 package com.github.norwae.whatiread.db;
 
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -9,20 +8,27 @@ import android.content.Context;
 public class BookDatabase {
 
 	private BookDatabaseHelper bookDatabaseAccess;
-
-	
+	private Context ctx;
 	
 	public BookDatabase(Context context) {
 		bookDatabaseAccess = new BookDatabaseHelper(context);
+		ctx = context;
 	}
 	
 
 	public BookInfo getForEAN13(String ean13) {
+		List<BookInfo> infos = bookDatabaseAccess.selectBookInfo(ctx, BookDatabaseHelper.ISBN_COLUMN + " == ?", ean13);
+		
+		if (!infos.isEmpty()) {
+			return infos.get(0);
+		}
+		
 		return new BookInfo("The Hook Mountain Massacre", "Nicolas Louge", "9781601250384", new Date(), 4, "Gore-drenched but gripping");
 	}
 	
 
-	public List<BookInfo> getForString(String string) {
-		return Collections.emptyList();
+	public List<BookInfo> getForString(String arg) {
+		String arg2 = "%" + arg + "%";
+		return bookDatabaseAccess.selectBookInfo(ctx, BookDatabaseHelper.TITLE_COLUMN + " LIKE ? OR " + BookDatabaseHelper.AUTHOR_COLUMN + " LIKE ?", arg2, arg2);
 	}
 }
--- src/com/github/norwae/whatiread/db/BookDatabaseHelper.java
+++ src/com/github/norwae/whatiread/db/BookDatabaseHelper.java
@@ -1,23 +1,42 @@
 package com.github.norwae.whatiread.db;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.text.format.DateFormat;
+import android.util.Log;
 
 public class BookDatabaseHelper extends SQLiteOpenHelper {
 
+
+	private static final int DATABASE_VERSION = 1;
 
 	private static final String DATABASE_NAME = "com.github.norwae.whatiread.db.BookDatabase";
 
 	private static final String BOOKSHELF_TABLE_NAME = "bookshelf";
 	
-	private static final String ISBN_COLUMN = "isbn";
-	private static final String AUTHOR_COLUMN = "author";
-	private static final String RATING_COLUMN = "rating";
-	private static final String SCANNED_COLUMN = "scanned";
-	private static final String COMMENT_COLUMN = "comment";
-	private static final String TITLE_COLUMN = "title";
+	static final String ISBN_COLUMN = "isbn";
+	static final String AUTHOR_COLUMN = "author";
+	static final String RATING_COLUMN = "rating";
+	static final String SCANNED_COLUMN = "scanned";
+	static final String COMMENT_COLUMN = "comment";
+	static final String TITLE_COLUMN = "title";
+	
+	private static final String[] BOOKSHELF_FIELD_NAMES = {
+		ISBN_COLUMN,
+		AUTHOR_COLUMN,
+		RATING_COLUMN,
+		SCANNED_COLUMN, 
+		COMMENT_COLUMN,
+		TITLE_COLUMN
+	};
 	
 	private static final String CREATE_BOOKINFO_TABLE = "CREATE TABLE " + BOOKSHELF_TABLE_NAME + " ("
 			+ ISBN_COLUMN + " TEXT,"
@@ -25,10 +44,10 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
 			+ AUTHOR_COLUMN + " TEXT,"
 			+ RATING_COLUMN + " INTEGER,"
 			+ COMMENT_COLUMN + " TEXT,"
-			+ SCANNED_COLUMN + " REAL);";
+			+ SCANNED_COLUMN + " TEXT);";
 
 	public BookDatabaseHelper(Context context) {
-		super(context, DATABASE_NAME, null, 0);
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 	
 	@Override
@@ -40,6 +59,41 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// NOT versioned right now
 
+	}
+
+	public List<BookInfo> selectBookInfo(Context ctx, String aQuery, String ...someQueryArgs) {
+		SQLiteDatabase database = getReadableDatabase();
+		Cursor cursor = database.query(BOOKSHELF_TABLE_NAME, BOOKSHELF_FIELD_NAMES, aQuery, someQueryArgs, null, null, null);
+
+		int authorIdx = cursor.getColumnIndex(AUTHOR_COLUMN);
+		int titleIdx = cursor.getColumnIndex(TITLE_COLUMN);
+		int isbnIdx = cursor.getColumnIndex(ISBN_COLUMN);
+		int commentIdx = cursor.getColumnIndex(COMMENT_COLUMN);
+		int scannedIdx = cursor.getColumnIndex(SCANNED_COLUMN);
+		int ratingIdx = cursor.getColumnIndex(RATING_COLUMN);
+		
+		List<BookInfo> rv = new ArrayList<BookInfo>();
+		
+		while (cursor.moveToNext()) {
+			String author = cursor.getString(authorIdx);
+			String title = cursor.getString(titleIdx);
+			String isbn = cursor.getString(isbnIdx);
+			String comment = cursor.getString(commentIdx);
+			Date scanned;
+			try {
+				scanned = DateFormat.getDateFormat(ctx).parse(cursor.getString(scannedIdx));
+			} catch (ParseException e) {
+				Log.e("Book Database", "Failed to convert stored scan-date", e);
+				scanned = new Date();
+			}
+			int rating = cursor.getInt(ratingIdx);
+			
+			BookInfo info = new BookInfo(title, author, isbn, scanned, rating, comment);
+			
+			rv.add(info);
+		}
+		
+		return rv;
 	}
 
 }
