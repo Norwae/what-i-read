c280e3554298a7b7f8f5e4aa283573dc7176fce9
Stefan Schulz <schulz.stefan@gmail.com>
2013-10-18 14:30:23 +0200

    Adding passing to barcode scanner
    Also, first time the app reached my phone. Yaaaay ;)

--- src/com/github/norwae/whatiread/MainActivity.java
+++ src/com/github/norwae/whatiread/MainActivity.java
@@ -1,22 +1,56 @@
 package com.github.norwae.whatiread;
 
+import java.util.Collections;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	private static final String EAN_13_TYPE = "EAN_13";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button b = (Button) findViewById(R.id.scan);
+		b.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				scanPressed();
+			}
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void scanPressed() {
+		IntentIntegrator scanIntent = new IntentIntegrator(this);
+		
+		scanIntent.initiateScan(Collections.singleton(EAN_13_TYPE));
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+		if (scanResult != null) {
+			
+		} else {
+			super.onActivityResult(requestCode, resultCode, data);
+		}
 	}
 
 }
