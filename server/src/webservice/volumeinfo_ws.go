b5daa7acc6503f5176a2e1254925e699207fe96b
Stefan Schulz <schulz.stefan@gmail.com>
2014-01-22 22:40:34 +0100

    Rewrote WS with somewhat better abstraction
    Now we don't pass around quite so many moving parts

--- server/src/webservice/volumeinfo_ws.go
+++ server/src/webservice/volumeinfo_ws.go
@@ -13,12 +13,150 @@ import (
 	"persistence"
 )
 
-func init() {
-	http.HandleFunc("/volumes/", serveLookup)
-	http.HandleFunc("/volumes", serveImportExport)
+type Call struct {
+	Context  ae.Context
+	Request  *http.Request
+	Response http.ResponseWriter
 }
 
-func LookupISBN(ctx ae.Context, country string, isbn isbn13.ISBN13) (resp *data.BookMetaData, err error) {
+func (call *Call) ReportError(err error) {
+	if me, ok := err.(ae.MultiError); ok {
+		for _, next := range me {
+			call.ReportError(next)
+		}
+	} else {
+		call.Context.Errorf("Error reported: %s", err.Error())
+	}
+}
+
+func (call *Call) DetermineCountry() string {
+	header := call.Request.Header["X-AppEngine-Country"]
+	if len(header) > 0 {
+		return header[0]
+	}
+
+	query := call.Request.URL.Query()["country"]
+
+	if len(query) > 0 {
+		return query[0]
+	}
+
+	return "unknown"
+}
+
+type CallHandler func(*Call) error
+
+func (function CallHandler) ServeHTTP(w http.ResponseWriter, rq *http.Request) {
+	call := Call{
+		Context:  ae.NewContext(rq),
+		Request:  rq,
+		Response: w,
+	}
+
+	err := function(&call)
+
+	if err != nil {
+		call.ReportError(err)
+	}
+}
+
+func init() {
+	http.Handle("/volumes/", CallHandler(serveVolumeSingle))
+	http.Handle("/volumes", CallHandler(serveVolumeBulk))
+}
+
+func serveVolumeBulk(call *Call) (err error) {
+	var shelf *data.Bookshelf
+	status := http.StatusInternalServerError
+	switch call.Request.Method {
+	case "GET":
+		shelf, err = persistence.LookupBookshelf(call.Context)
+	case "PUT":
+		shelf, err = putVolumeBulk(call)
+	default:
+		status = http.StatusMethodNotAllowed
+		err = errors.New("Unsupported operation. Only GET, PUT and DELETE methods are allowed")
+	}
+
+	if err == nil {
+		encode := json.NewEncoder(call.Response)
+		call.Response.Header().Add("Content-Type", "application/json")
+		call.Response.WriteHeader(http.StatusOK)
+		err = encode.Encode(shelf)
+	} else {
+		call.Response.WriteHeader(status)
+	}
+
+	return err
+}
+
+func putVolumeBulk(call *Call) (shelf *data.Bookshelf, err error) {
+	decode := json.NewDecoder(call.Request.Body)
+	shelf = new(data.Bookshelf)
+	if err = decode.Decode(shelf); err == nil {
+		err = persistence.StoreBookshelf(call.Context, shelf)
+	}
+
+	return
+}
+
+func serveVolumeSingle(call *Call) error {
+	status := http.StatusBadRequest
+	isbn, err := isbn13.New(call.Request.URL.Path[9:])
+	var book *data.BookMetaData
+
+	if err == nil {
+		status = http.StatusInternalServerError
+		switch call.Request.Method {
+		case "GET":
+			book, err = compositeISBNLookup(call.Context, call.DetermineCountry(), isbn)
+		case "PUT":
+			book, err = putVolumeSingle(call, isbn)
+		default:
+			status = http.StatusMethodNotAllowed
+			err = errors.New("Unsupported operation. Only GET, PUT and DELETE methods are allowed")
+		}
+	}
+
+	if err == nil {
+		encode := json.NewEncoder(call.Response)
+
+		call.Response.Header().Add("Content-Type", "application/json")
+		call.Response.WriteHeader(http.StatusOK)
+		err = encode.Encode(book)
+	} else {
+		call.Response.WriteHeader(status)
+	}
+
+	return err
+}
+
+func putVolumeSingle(call *Call, isbn isbn13.ISBN13) (*data.BookMetaData, error) {
+	shelf, err := persistence.LookupBookshelf(call.Context)
+	var info *data.BookMetaData
+
+	if err == ds.ErrNoSuchEntity {
+		err = nil
+		shelf = new(data.Bookshelf)
+	}
+
+	if err == nil {
+		decode := json.NewDecoder(call.Request.Body)
+		info = new(data.BookMetaData)
+		if err = decode.Decode(info); err == nil {
+			if ptr, _ := shelf.LookupInfo(isbn); ptr != nil {
+				*ptr = *info
+			} else {
+				shelf.Books = append(shelf.Books, *info)
+			}
+
+			err = persistence.StoreBookshelf(call.Context, shelf)
+		}
+	}
+	return info, err
+}
+
+func compositeISBNLookup(ctx ae.Context, country string, isbn isbn13.ISBN13) (resp *data.BookMetaData, err error) {
 	funcs := []func(ae.Context, string, isbn13.ISBN13) (*data.BookMetaData, error){
 		func(ctx ae.Context, country string, isbn isbn13.ISBN13) (*data.BookMetaData, error) {
 			shelf, err := persistence.LookupBookshelf(ctx)
@@ -54,128 +192,4 @@ func LookupISBN(ctx ae.Context, country string, isbn isbn13.ISBN13) (resp *data.
 	}
 
 	return nil, multi
-}
-
-func serveImportExport(w http.ResponseWriter, rq *http.Request) {
-	ctx := ae.NewContext(rq)
-	var err error
-	var shelf *data.Bookshelf
-	status := http.StatusBadRequest
-
-	switch rq.Method {
-	case "GET":
-		shelf, err = persistence.LookupBookshelf(ctx)
-	case "PUT":
-		decode := json.NewDecoder(rq.Body)
-		shelf = new(data.Bookshelf)
-		if err = decode.Decode(shelf); err == nil {
-			status = http.StatusInternalServerError
-			err = persistence.StoreBookshelf(ctx, shelf)
-		}
-	}
-
-	if err != nil {
-		reportError(err)
-		w.WriteHeader(status)
-	} else {
-		encode := json.NewEncoder(w)
-		w.WriteHeader(http.StatusOK)
-		w.Header().Add("Content-Type", "application/json")
-		if err = encode.Encode(shelf); err != nil {
-			ctx.Errorf("Could not report error on encode: %s\n", err.Error())
-		}
-
-	}
-}
-
-func serveLookup(w http.ResponseWriter, rq *http.Request) {
-	isbn, err := isbn13.New(rq.URL.Path[9:])
-	status := http.StatusBadRequest
-
-	if err == nil {
-		status = http.StatusInternalServerError
-		switch rq.Method {
-		case "GET":
-			err = handleGet(w, rq, isbn)
-		case "PUT":
-			err = handlePut(w, rq, isbn)
-		default:
-			status = http.StatusMethodNotAllowed
-			err = errors.New("Unsupported operation. Only GET, PUT and DELETE methods are allowed")
-		}
-
-	}
-
-	if err != nil {
-		reportError(err)
-		w.WriteHeader(status)
-	}
-}
-
-func reportError(e error) {
-	if me, ok := e.(ae.MultiError); ok {
-		for _, next := range me {
-			reportError(next)
-		}
-	} else {
-		ctx.Errorf("Error reported: %s", e.Error())
-	}
-}
-
-func handlePut(w http.ResponseWriter, rq *http.Request, isbn isbn13.ISBN13) error {
-	ctx := ae.NewContext(rq)
-	shelf, err := persistence.LookupBookshelf(ctx)
-
-	if err == ds.ErrNoSuchEntity {
-		err = nil
-		shelf = new(data.Bookshelf)
-	}
-
-	if err == nil {
-		decode := json.NewDecoder(rq.Body)
-		info := new(data.BookMetaData)
-		if err = decode.Decode(info); err == nil {
-			if ptr, _ := shelf.LookupInfo(isbn); ptr != nil {
-				*ptr = *info
-			} else {
-				shelf.Books = append(shelf.Books, *info)
-			}
-
-			err = persistence.StoreBookshelf(ctx, shelf)
-		}
-	}
-	return err
-
-}
-
-func handleGet(w http.ResponseWriter, rq *http.Request, isbn isbn13.ISBN13) error {
-	var reply *data.BookMetaData
-	var err error
-	ctx := ae.NewContext(rq)
-	country := determineCountry(rq)
-	if reply, err = LookupISBN(ctx, country, isbn); err == nil {
-		encode := json.NewEncoder(w)
-		w.WriteHeader(http.StatusOK)
-		w.Header().Add("Content-Type", "application/json")
-		if err = encode.Encode(reply); err == nil {
-			return nil
-		}
-	}
-
-	return err
-}
-
-func determineCountry(rq *http.Request) string {
-	header := rq.Header["X-AppEngine-Country"]
-	if len(header) > 0 {
-		return header[0]
-	}
-	query := rq.URL.Query()["country"]
-
-	if len(query) > 0 {
-		return query[0]
-	}
-
-	return "unknown"
-
 }
