2ba24f90511a2556633948664357ab831dd22871
Stefan Schulz <schulz.stefan@gmail.com>
2014-02-16 22:27:19 +0100

    Minor fixes for version 10 (ready for prime time)

--- client/src/com/github/norwae/whatiread/DisplayBookActivity.java
+++ client/src/com/github/norwae/whatiread/DisplayBookActivity.java
@@ -6,9 +6,6 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.norwae.whatiread.data.BookInfo;
