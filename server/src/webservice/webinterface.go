5d5e7c7fb0ef8488f6f9760dfe0f1b3377fd6905
Stefan Schulz <schulz.stefan@gmail.com>
2014-05-16 22:36:36 +0200

    working page (still no list)

--- server/src/webservice/webinterface.go
+++ server/src/webservice/webinterface.go
@@ -1,33 +1,33 @@
 package webservice
 
 import (
-	"net/http"
-	"html/template"
 	"appengine"
+	"html/template"
+	"net/http"
 )
 
-var searchPage = template.Must(template.ParseFiles("tmpl/main.htmlt", "tmpl/basics.htmlt"))
-// var bookPage = template.Must(template.ParseFiles("tmpl/book.htmlt"))
-var errorPage = template.Must(template.ParseFiles("tmpl/error.htmlt", "tmpl/basics.htmlt"))
+var searchPage, errorPage *template.Template
 
 func init() {
 	http.Handle("/", http.RedirectHandler("/index.html", http.StatusMovedPermanently))
 	http.Handle("/index.html", http.HandlerFunc(serveStartPage))
 	// http.Handle("/book/", http.HandlerFunc(serveBookPage))
+
+	searchPage = template.Must(template.ParseFiles("tmpl/main.html"))
 }
 
 func serveStartPage(rsp http.ResponseWriter, rq *http.Request) {
-	call := Call {
-		Context:appengine.NewContext(rq),
-		Request: rq,
-		Response: rsp, 
+	call := Call{
+		Context:  appengine.NewContext(rq),
+		Request:  rq,
+		Response: rsp,
 	}
-	
-	if bookInfos, err := getVolumeBulk(&call); err == nil {
-		call.Context.Infof("Emitting searchPage template")
-		searchPage.Execute(rsp, bookInfos)
+
+	if info, err := getVolumeBulk(&call); err == nil {
+		call.Context.Infof("Emitting searchPage template: %v", searchPage.Execute(rsp, info))
 	} else {
 		call.Context.Errorf("Emitting error template (%s)", err)
+		rsp.WriteHeader(http.StatusInternalServerError)
 		errorPage.Execute(rsp, err)
 	}
 }
